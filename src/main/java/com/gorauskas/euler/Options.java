b85945b8b15501363e39606221010e50d0f70788
Jonas Gorauskas <jgorauskas@gmail.com>
2015-06-22 21:32:40 -0800

    adding options class to handle cmd line params

--- src/main/java/com/gorauskas/euler/Options.java
+++ src/main/java/com/gorauskas/euler/Options.java
@@ -1,5 +1,33 @@
 package com.gorauskas.euler;
 
+import org.kohsuke.args4j.Option;
 
 public class Options {
+    @Option(name = "-V", usage = "Displays the version information and exits.")
+    private boolean showVersion;
+
+    @Option(name = "-h", aliases = {"--help", "-?"}, usage = "Displays this help message and exits.")
+    private boolean showUsage;
+
+    @Option(name = "-v", usage = "Verbose outputs the problem statement and the solution.")
+    private boolean verbose;
+
+    @Option(name = "-p", aliases = {"--problem"}, usage = "Specify the problem/solution number you want to run.")
+    private int problem;
+
+    public int getProblem() {
+        return problem;
+    }
+
+    public boolean isVerbose() {
+        return verbose;
+    }
+
+    public boolean doShowVersion() {
+        return showVersion;
+    }
+
+    public boolean doShowUsage() {
+        return showUsage;
+    }
 }
