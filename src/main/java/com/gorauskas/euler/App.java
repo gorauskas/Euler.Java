b6575074f569d911d2d93a10ede2bfcd5cf312b6
Jonas Gorauskas <jgorauskas@gmail.com>
2015-06-22 21:54:12 -0800

    making the main method a class loader

--- src/main/java/com/gorauskas/euler/App.java
+++ src/main/java/com/gorauskas/euler/App.java
@@ -1,13 +1,43 @@
 package com.gorauskas.euler;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.kohsuke.args4j.CmdLineException;
+
+public class App {
+
+    private static String EULER_PACKAGE = "com.gorauskas.euler.solutions.";
+    private static String EULER_CLASS = "Euler";
+
+    public static void main( String[] args ) {
+
+        try {
+
+            Util.parseOptions(args);
+            ClassLoader cl = App.class.getClassLoader();
+            Euler euler = (Euler) cl
+                    .loadClass(App.EULER_PACKAGE + App.EULER_CLASS + Util.problem)
+                    .newInstance();
+
+            if (Util.verbose) {
+                System.out.println(euler.getProblem());
+            }
+
+            System.out.println(euler.getAnswer());
+
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+
+            System.err.println("Unable to load Euler Problem class.");
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, e);
+            System.exit(1);
+
+        } catch (CmdLineException ex) {
+
+            System.err.println("Unable to parse command line parameters.");
+            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            System.exit(1);
+
+        }
+
     }
 }
