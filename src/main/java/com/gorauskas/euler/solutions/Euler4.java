e31c28a120f2a0fd20a15079c6dc251652603550
Jonas Gorauskas <jgorauskas@gmail.com>
2015-06-27 10:30:10 -0800

    adding problem 4 solution

---
+++ src/main/java/com/gorauskas/euler/solutions/Euler4.java
@@ -0,0 +1,48 @@
+package com.gorauskas.euler.solutions;
+
+import com.gorauskas.euler.Euler;
+import com.gorauskas.euler.Util;
+import java.util.ArrayList;
+
+public class Euler4 implements Euler {
+
+    @Override
+    public double Solve() {
+        long x = 0;
+        long y = 0;
+
+        for (int i=999; i > 99; i--) {
+            for (int j = i; j > 99; j--) {
+                x = i * j;
+
+                if (Util.isPalindrome(x)) {
+                    if (x > y) {
+                        y = x;
+                    }
+                }
+            }
+        }
+
+        return (double) y;
+    }
+
+    @Override
+    public String getProblem() {
+        String s = new StringBuilder()
+                .append("Project Euler Problem 4:\n")
+                .append("\n")
+                .append("   A palindromic number reads the same both ways. The largest palindrome made\n")
+                .append("   from the product of two 2-digit numbers is 9009 = 91  99.\n")
+                .append("\n")
+                .append("   Find the largest palindrome made from the product of two 3-digit numbers.\n")
+                .toString();
+        return s;
+    }
+
+    @Override
+    public String getAnswer() {
+        String s = String.format("The largest palindrome made of the product of two 3 digit numbers is %s"
+                , this.Solve());
+        return s;
+    }
+}
