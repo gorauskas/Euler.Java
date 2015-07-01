3e7d23c86e17e91fa4a5666d774d6bfdc6cad66b
Jonas Gorauskas <jgorauskas@gmail.com>
2015-06-30 18:30:00 -0800

    adding problem 7 solution

---
+++ src/main/java/com/gorauskas/euler/solutions/Euler7.java
@@ -0,0 +1,42 @@
+package com.gorauskas.euler.solutions;
+
+import com.gorauskas.euler.Euler;
+
+public class Euler7 implements Euler {
+
+    @Override
+    public double Solve() {
+        long n = 0, p = 1;
+
+        while (p < 10000) {
+            ++n;
+            for (int i = 2; i <= (long)Math.sqrt(n) && n % i != 0; i++) {
+                if (i == (int)Math.sqrt(n)) {
+                    p++;
+                }
+            }
+        }
+
+        return n;
+    }
+
+    @Override
+    public String getProblem() {
+        String s = new StringBuilder()
+                .append("Project Euler Problem 7:\n")
+                .append("\n")
+                .append("   By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13,\n")
+                .append("   we can see that the 6th prime is 13.\n")
+                .append("\n")
+                .append("   What is the 10001st prime number?\n")
+                .toString();
+        return s;
+    }
+
+    @Override
+    public String getAnswer() {
+        String s = String.format("Prime 10001 is %s"
+                , this.Solve());
+        return s;
+    }
+}
