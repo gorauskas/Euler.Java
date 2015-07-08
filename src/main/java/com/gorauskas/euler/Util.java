54652e05dbad0e3a682cc37db36dcda3f5313143
Jonas Gorauskas <jgorauskas@gmail.com>
2015-07-07 18:13:00 -0800

    adding utility stream functions

--- src/main/java/com/gorauskas/euler/Util.java
+++ src/main/java/com/gorauskas/euler/Util.java
@@ -3,8 +3,11 @@ package com.gorauskas.euler;
 import org.kohsuke.args4j.CmdLineException;
 import org.kohsuke.args4j.CmdLineParser;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 public final class Util {
     public static boolean verbose = false;
@@ -104,5 +107,50 @@ public final class Util {
 
     public static boolean isMultipleOf(long num, LongStream multiples) {
         return multiples.allMatch(n -> isMultipleOf(num, n));
+    }
+
+    public static IntStream primeSequence(int max) {
+        List<Integer> primes = new ArrayList<>(max);
+        return IntStream.iterate(2, i -> i + 1)
+                .filter(x -> {
+                    for (int prime: primes)
+                        if (x % prime == 0)
+                            return false;
+                    return true;
+                })
+                .limit(max);
+    }
+
+    public static IntStream primeSequence() {
+        return primeSequence(Integer.MAX_VALUE);
+    }
+
+    public static IntStream fibonacciSequence(int max) {
+        return Stream.iterate(new int[]{1, 1}, f -> new int[]{f[1], f[0] + f[1]})
+                .mapToInt(f -> f[0])
+                .limit(max);
+    }
+
+    public static IntStream fibonacciSequence() {
+        return fibonacciSequence(Integer.MAX_VALUE);
+    }
+
+    public static IntStream triangleNumberSequence(int max) {
+        return IntStream.iterate(1, i -> i + 1)
+                .map(n -> (n * n + n) / 2)
+                .limit(max);
+    }
+
+    public static IntStream triangleNumberSequence() {
+        return triangleNumberSequence(Integer.MAX_VALUE);
+    }
+
+    public static int numberOfDivisors(int n) {
+        if (n == 1)
+            return 1;
+
+        return (int)IntStream.rangeClosed(1, (int)Math.sqrt((double)n))
+                .filter(x -> isMultipleOf(n, x))
+                .count() * 2;
     }
 }
