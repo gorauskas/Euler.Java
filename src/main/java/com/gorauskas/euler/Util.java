ee94672baf500299dd47d40674a4304c1fd73308
Jonas Gorauskas <jgorauskas@gmail.com>
2018-06-03 10:04:51 -0800

    moved gcd function to Util

--- src/main/java/com/gorauskas/euler/Util.java
+++ src/main/java/com/gorauskas/euler/Util.java
@@ -157,4 +157,8 @@ public final class Util {
                 .filter(i -> n % i == 0)
                 .sum();
     }
+
+    public static long gcd(long a, long b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
 }
