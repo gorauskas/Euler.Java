f53efa5a849a11a743872eec68993afa56e44e5f
Jonas Gorauskas <jgorauskas@gmail.com>
2019-02-18 22:27:52 -0800

    clean up for jdk 11

--- src/main/java/com/gorauskas/euler/solutions/Euler1.java
+++ src/main/java/com/gorauskas/euler/solutions/Euler1.java
@@ -31,7 +31,7 @@ public class Euler1 implements Euler {
 
     @Override
     public String getProblem() {
-        String s = new StringBuilder()
+        return new StringBuilder()
                 .append("Project Euler Problem 1:\n")
                 .append("\n")
                 .append("   If we list all the natural numbers below 10 that are\n")
@@ -40,14 +40,11 @@ public class Euler1 implements Euler {
                 .append("\n")
                 .append("   Find the sum of all the multiples of 3 or 5 below 1000.\n")
                 .toString();
-
-        return s;
     }
 
     @Override
     public String getAnswer() {
-        String s = String.format("The sum of all the multiples of 3 or 5 below 1000 is: %s"
+        return String.format("The sum of all the multiples of 3 or 5 below 1000 is: %s"
                 , this.Solve());
-        return s;
     }
 }
