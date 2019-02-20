48d4672058961d3a24e2e5bbe7a57f45b89d196f
Jonas Gorauskas <jgorauskas@gmail.com>
2019-02-19 22:09:48 -0800

    clean up for jdk 11

--- src/main/java/com/gorauskas/euler/solutions/Euler10.java
+++ src/main/java/com/gorauskas/euler/solutions/Euler10.java
@@ -1,38 +1,31 @@
 package com.gorauskas.euler.solutions;
 
 import com.gorauskas.euler.Euler;
-import com.gorauskas.euler.Util;
 
-import java.util.stream.LongStream;
 import com.gorauskas.euler.streams.StreamUtil;
 
 public class Euler10 implements Euler {
 
     @Override
     public double Solve() {
-
-        // return LongStream.range(2, 2000000).filter(Util::isPrime).sum();
-
         return StreamUtil.primeSequence(2000000).sum();
     }
 
     @Override
     public String getProblem() {
-        String s = new StringBuilder()
+        return new StringBuilder()
                 .append("Project Euler Problem 10:\n")
                 .append("\n")
                 .append("   The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.\n")
                 .append("\n")
                 .append("   Find the sum of all the primes below two million.\n")
                 .toString();
-        return s;
     }
 
     @Override
     public String getAnswer() {
-        String s = String.format("The sum of all the primes below two million is: %s"
+        return String.format("The sum of all the primes below two million is: %s"
                 , (long)this.Solve());
-        return s;
     }
 
 }
