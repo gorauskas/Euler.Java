96475c03e3e9fef6978a62ea4ad8204cdacf15a0
Jonas Gorauskas <jgorauskas@gmail.com>
2019-02-21 18:00:28 -0800

    clean up for jdk 11

--- src/main/java/com/gorauskas/euler/solutions/Euler16.java
+++ src/main/java/com/gorauskas/euler/solutions/Euler16.java
@@ -13,20 +13,18 @@ public class Euler16 implements Euler {
 
     @Override
     public String getProblem() {
-        String s = new StringBuilder()
+        return new StringBuilder()
                 .append("Project Euler Problem 16:\n")
                 .append("\n")
                 .append("    2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.\n")
                 .append("\n")
                 .append("    What is the sum of the digits of the number 2^1000?\n")
                 .toString();
-        return s;
     }
 
     @Override
     public String getAnswer() {
-        String s = String.format("The sum of the digits of the number 2^1000 is %s"
+        return String.format("The sum of the digits of the number 2^1000 is %s"
                 , (long)this.Solve());
-        return s;
     }
 }
